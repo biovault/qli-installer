1979e9e2513bc90191d2a9b6c687b7de01191f4a
Baldur van Lew <baldurvanlew@gmail.com>
2020-02-19 14:32:13 +0100

    Environment setting should be string

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ from bincrafters import build_template_default
 if __name__ == "__main__":
     
     if platform.system() == "Darwin":
-        os.environ['CONAN_SKIP_BROKEN_SYMLINKS_CHECK'] = 1
+        os.environ['CONAN_SKIP_BROKEN_SYMLINKS_CHECK'] = '1'
         
     builder = build_template_default.get_builder()
 
