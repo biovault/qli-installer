fdec31aaf73b6bb98e830aec269e494be7d19472
Baldur van Lew <baldurvanlew@gmail.com>
2020-02-19 13:06:45 +0100

    Fix typo

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
     
-    if playform.system() == "Darwin":
+    if platform.system() == "Darwin":
         os.environ['CONAN_SKIP_BROKEN_SYMLINKS_CHECK'] = 1
         
     builder = build_template_default.get_builder()
