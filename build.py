d6341ce982067bf67595192865f2383b64b28e33
Baldur van Lew <baldurvanlew@gmail.com>
2020-02-19 14:54:38 +0100

    Move broken link check disable to the travis for MacOS only

--- build.py
+++ build.py
@@ -7,10 +7,7 @@ import platform
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    
-    if platform.system() == "Darwin":
-        os.environ['CONAN_SKIP_BROKEN_SYMLINKS_CHECK'] = '1'
-        
+           
     builder = build_template_default.get_builder()
 
     builder.run()
