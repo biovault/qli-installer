a13fde8838722fc8542c692f8c065a1a6a2ff56a
Baldur van Lew <baldurvanlew@gmail.com>
2020-09-16 13:29:45 +0200

    Change to 5.15.0
    Because the msvc2017 is not yet available for 5.15.1

--- conanfile.py
+++ conanfile.py
@@ -53,7 +53,7 @@ class QtConan(ConanFile):
         "purchasing"]
 
     name = "qt"
-    version = "5.15.1"
+    version = "5.15.0"
     description = "Qt is a cross-platform framework for graphical user interfaces."
     topics = ("conan", "qt", "ui")
     url = "https://github.com/bldrvnlw/qli-installer"
