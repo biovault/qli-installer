51d7ccff305eb3e3e054b94963ffef5888e453bf
Baldur van Lew <baldurvanlew@gmail.com>
2020-09-16 13:04:28 +0200

    Update pip as part of install

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,21 +4,23 @@ set -ex
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     unset PYENV_ROOT
-    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-    export PATH="$HOME/.pyenv/bin:$PATH"
+    brew update
+    brew install openssl readline
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
 
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
-
+    
     pyenv install 3.7.1
     pyenv virtualenv 3.7.1 conan
     pyenv rehash
     pyenv activate conan
 
-    pip install cmake --upgrade
 fi
 
+pip install --upgrade pip
 pip install conan --upgrade
 pip install conan_package_tools bincrafters_package_tools
 
