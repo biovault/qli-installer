6d07eefad4e9f8554d24722a3cca96cae265040e
B. van Lew <b.van_lew@lumc.nl>
2020-10-05 13:06:51 +0200

    Initial checking for qt5.14.2
    5.14.1 build package build breaks due to WaylandCompositor mistake

--- conanfile.py
+++ conanfile.py
@@ -53,7 +53,7 @@ class QtConan(ConanFile):
         "purchasing"]
 
     name = "qt"
-    version = "5.14.1"
+    version = "5.14.2"
     description = "Qt is a cross-platform framework for graphical user interfaces."
     topics = ("conan", "qt", "ui")
     url = "https://github.com/bldrvnlw/qli-installer"
