0bc610e463eb3d7a9a1478a662d4c8189b507b3d
B. van Lew <b.van_lew@lumc.nl>
2020-10-26 09:28:30 +0100

    Fix the p7zip install (required by qliinstaller)

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,15 +4,14 @@ set -ex
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     unset PYENV_ROOT
-    export HOMEBREW_NO_AUTO_UPDATE=1
-    brew install openssl readline
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
+    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+    export PATH="$HOME/.pyenv/bin:$PATH"
 
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
     
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install p7zip
     pyenv install 3.7.1
     pyenv virtualenv 3.7.1 conan
     pyenv rehash
