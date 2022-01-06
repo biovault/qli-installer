c38c9c330447a106e450d369f0c79254b26e2e60
B. van Lew <b.van_lew@lumc.nl>
2022-01-06 16:29:27 +0100

    Test the build for qt5.12.2

--- conanfile.py
+++ conanfile.py
@@ -31,6 +31,7 @@ from conans.errors import ConanInvalidConfiguration
 from conans.model import Generator
 from installutils import install_qt
 
+
 class qt(Generator):
     @property
     def filename(self):
@@ -38,22 +39,26 @@ class qt(Generator):
 
     @property
     def content(self):
-        return "[Paths]\nPrefix = %s" % self.conanfile.deps_cpp_info["qt"].rootpath.replace("\\", "/")
+        return "[Paths]\nPrefix = %s" % self.conanfile.deps_cpp_info[
+            "qt"
+        ].rootpath.replace("\\", "/")
 
 
 class QtConan(ConanFile):
 
-    _submodules = ["webengine", 
-        "webglplugin", 
-        "virtualkeyboard", 
+    _submodules = [
+        "webengine",
+        "webglplugin",
+        "virtualkeyboard",
         "script",
-        "datavis3d", 
-        "charts", 
-        "networkauth", 
-        "purchasing"]
+        "datavis3d",
+        "charts",
+        "networkauth",
+        "purchasing",
+    ]
 
     name = "qt"
-    version = "5.15.1"
+    version = "5.15.2"
     description = "Qt is a cross-platform framework for graphical user interfaces."
     topics = ("conan", "qt", "ui")
     url = "https://github.com/bldrvnlw/qli-installer"
@@ -63,85 +68,97 @@ class QtConan(ConanFile):
     exports = ["LICENSE.md", "installutils.py", "__init__.py"]
     settings = "os", "arch", "compiler", "build_type"
 
-    options = dict({
-        "target": ["desktop", "android", "ios"],
-        "commercial": [True, False],
-        "opengl": ["no", "es2", "desktop", "dynamic"],
-        "openssl": [True, False],
-        "webengine": [True, False],
-        "webglplugin": [True, False],
-        "virtualkeyboard": [True, False],
-        "script": [True, False],
-        "datavis3d": [True, False], 
-        "charts": [True, False],
-        "networkauth": [True, False],
-        "purchasing": [True, False]       
-    })
+    options = dict(
+        {
+            "target": ["desktop", "android", "ios"],
+            "commercial": [True, False],
+            "opengl": ["no", "es2", "desktop", "dynamic"],
+            "openssl": [True, False],
+            "webengine": [True, False],
+            "webglplugin": [True, False],
+            "virtualkeyboard": [True, False],
+            "script": [True, False],
+            "datavis3d": [True, False],
+            "charts": [True, False],
+            "networkauth": [True, False],
+            "purchasing": [True, False],
+        }
+    )
     no_copy_source = True
-    default_options = dict({
-        "commercial": False,
-        "target": "desktop",
-        "opengl": "desktop",
-        "openssl": True,
-        "webengine": True,
-        "webglplugin": True,
-        "virtualkeyboard": True,
-        "script": True,
-        "datavis3d": True, 
-        "charts": True,
-        "networkauth": True,
-        "purchasing": True          
-    })
+    default_options = dict(
+        {
+            "commercial": False,
+            "target": "desktop",
+            "opengl": "desktop",
+            "openssl": True,
+            "webengine": True,
+            "webglplugin": True,
+            "virtualkeyboard": True,
+            "script": True,
+            "datavis3d": True,
+            "charts": True,
+            "networkauth": True,
+            "purchasing": True,
+        }
+    )
 
     short_paths = True
 
     def configure(self):
-    # Target platform specific stuff here
-        if self.settings.os == 'Linux':
+        # Target platform specific stuff here
+        if self.settings.os == "Linux":
             self.options.target = "desktop"
         if self.settings.os == "Windows":
-            self.options.target = "desktop" 
+            self.options.target = "desktop"
         if self.settings.os == "Macos":
-            self.options.target = "desktop" 
-            
+            self.options.target = "desktop"
 
     def build(self):
         common_args = dict({})
         os_args = dict({})
-        
-        os_map = dict({"Linux": "linux", "Windows": "windows", "Macos": "mac", "iOS": "mac"})
+
+        os_map = dict(
+            {"Linux": "linux", "Windows": "windows", "Macos": "mac", "iOS": "mac"}
+        )
         # Use concatenated  settings.os settings.compiler.version settings.arch
         # to get an architecture string for the QT install
-        arch_map = dict({
-            "Windows15x86": "win32_msvc2017", 
-            "Windows15x86_64": "win64_msvc2017_64", 
-            "Windows14x86": "win32_msvc2015", 
-            "Windows14x86_64": "win64_msvc2015_64",
-            "Linux": "gcc_64",
-            "Macos": "clang_64"
-        })
+        arch_map = dict(
+            {
+                "Windows15x86": "win32_msvc2017",
+                "Windows15x86_64": "win64_msvc2017_64",
+                "Windows14x86": "win32_msvc2015",
+                "Windows14x86_64": "win64_msvc2015_64",
+                "Linux": "gcc_64",
+                "Macos": "clang_64",
+            }
+        )
         print(self.settings.os)
         print(self.settings.compiler.version)
-        print(self.settings.arch)        
-        
-        if self.settings.os == 'Linux':
+        print(self.settings.arch)
+
+        if self.settings.os == "Linux":
             arch_key = "Linux"
         elif self.settings.os == "Macos":
             arch_key = "Macos"
         else:
-            arch_key = str(self.settings.os) + str(self.settings.compiler.version) + str(self.settings.arch)
-            
-        print(arch_key)    
+            arch_key = (
+                str(self.settings.os)
+                + str(self.settings.compiler.version)
+                + str(self.settings.arch)
+            )
+
+        print(arch_key)
         common_args["qt_version"] = self.version
         common_args["host_system"] = os_map[str(self.settings.get_safe("os"))]
-        common_args["packages"] = [pkg for pkg in self._submodules if self.options.get_safe(pkg)]
+        common_args["packages"] = [
+            pkg for pkg in self._submodules if self.options.get_safe(pkg)
+        ]
         print("Packages to installed: ", common_args["packages"])
         os_args["target"] = self.options.target
         os_args["arch"] = arch_map[arch_key]
-        
-        
+
         install_qt(common_args, os_args)
-        
+
     def package(self):
         self.copy("*")
 
